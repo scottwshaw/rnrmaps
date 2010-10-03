70d8cceb4804c907c6388d3c0073105106eb3e99
Scott Shaw <scottwshaw@gmail.com>
2010-10-03 14:28:50 +1100

    broke out gmaps functionality in the view into partials

--- lib/map.rb
+++ lib/map.rb
@@ -1,9 +1,15 @@
+class LatLng 
+  attr_reader :lat, :lng
+  def initialize(lat, lng)
+    @lat = lat
+    @lng = lng
+  end
+end
 class Map
-  attr_reader :center_lat, :center_lon, :zoom, :container_id
-  
-  def initialize(center_lat, center_lon, zoom, container_id)
-    @center_lat = center_lat
-    @center_lon = center_lon
+  attr_reader :center, :zoom, :container_id
+    
+  def initialize(center_lat, center_lng, zoom, container_id)
+    @center = LatLng.new(center_lat, center_lng)
     @zoom = zoom
     @container_id = container_id
   end
